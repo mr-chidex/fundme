f6d33f405918a79ca21f7f227e0cad2e8a7746cb
mr-chidex <mbahchiboy2@gmail.com>
2022-07-12 20:56:51 +0100

    auth signup done

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,33 @@
 import { RequestHandler } from 'express';
-// import { PrismaClient } from '@prisma/client';
-// import prisma from '../prisma/prisma';
+import bcrypt from 'bcrypt';
+
+import { User } from '../libs/types';
+import { validateSignup } from '../utils/auth.util';
+import prisma from '../prisma/prisma';
 
 export const signup: RequestHandler = async (req, res) => {
-  const { name, email, password } = req.body as { email: string; name: string; password: string };
-  console.log(name, email, password);
-  console.log(process.env.NODE_ENV);
-  res.json({ message: 'here' });
+  // validate request body
+  const { error, value } = validateSignup(req.body as User);
+  if (error) return res.status(422).json({ message: error.details[0].message });
+
+  const { name, email, password } = value;
+
+  // check if email is already in use
+  const userExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (userExist) return res.status(400).json({ status: 'error', message: 'email already in use', code: 400 });
+
+  // if user email is new
+  const salt = await bcrypt.genSalt(12);
+  const hasPassword = await bcrypt.hash(password, salt);
+
+  const user = await prisma.user.create({
+    data: { name, email, password: hasPassword },
+    select: { email: true, name: true },
+  });
+
+  res.status(201).json({ status: 'success', message: 'user signed up successfully', data: { user } });
 };
--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma: PrismaClient = global.prisma || new PrismaClient({ rejectOnNotFound: true });
+const prisma: PrismaClient = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') {
   if (!global.prisma) {
     global.prisma = prisma;
-    global.prisma.then(() => {
+    global.prisma.$connect().then(() => {
       console.log('Database connected successfully');
     });
   }
